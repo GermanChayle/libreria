f4f4893511a05bf371393d5f48c2552cd2b46168
GermanChayle <ger.chayle@gmail.com>
2024-11-18 16:28:38 -0300

    Agrega existsByAutores y existsByEditorial a LibroRepositorio

--- src/main/java/com/example/libreria/repository/LibroRepositorio.java
+++ src/main/java/com/example/libreria/repository/LibroRepositorio.java
@@ -19,6 +19,8 @@ public interface LibroRepositorio extends JpaRepository<Libro, Long> {
     Optional<Libro> findByIsbnAndAltaFalse(Long isbn);
     Optional<Libro> findByTitulo(String titulo);
     Optional<Libro> findByTituloAndAltaTrue(String titulo);
+    boolean existsByAutores(Autor autor);
+    boolean existsByEditorial(Editorial editorial);
     boolean existsByAutoresAndAltaTrue(Autor autor);
     boolean existsByEditorialAndAltaTrue(Editorial editorial);
 }
